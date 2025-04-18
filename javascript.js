9a0ca7fe775ad8907d6246f086e24ec25aa8ca4f
TheREAl1legend <117980266+TheREAl1legend@users.noreply.github.com>
2025-04-18 11:18:21 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let arr1 = ["html", "css", "js", "react", "You are doing great!"];
+console.log(arr1[4]);
+
+let student={
+    studentname:"Gurpratik",
+    age:"19",
+    phonenumber:"85448474628",
+    city:"Chandigarh"
+};
+
+console.log(student.phonenumber);
+
+
+let great = () => {
+    console.log("Perfect!");
+}
+
+//JSON -> javascript object notation; client sends request to open home page 
+
